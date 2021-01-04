e07d57689cb86150d4cdc4d215e06e0f5617d877
José Nieto <jose@xiam.io>
2021-01-04 07:40:15 -0600

    add package description

---
+++ sexpr.go
@@ -0,0 +1,5 @@
+// The sexpr package is a Go library that provides a general purpose
+// S-expression parser and a lexer. The lexer scans a stream of bytes for a
+// finite set of patterns (tokens) and identifies lexemes, the parser takes
+// those lexemes as input and builds abstract syntax trees (ASTs) with them.
+package sexpr
